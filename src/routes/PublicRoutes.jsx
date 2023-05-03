1b73b7948f12338d6e726b7172ddf05ba7bd8aa5
Goodluck Kingsley <goodluckcanhelp@gmail.com>
2023-05-03 12:07:37 +0100

    director

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const PublicRoutes = () => {
     return (
@@ -8,6 +9,8 @@ const PublicRoutes = () => {
             <Navbar />
 
             <Outlet />
+
+            <Footer />
         </div>
     );
 };
